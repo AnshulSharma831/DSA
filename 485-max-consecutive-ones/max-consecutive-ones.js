7c3cbe6c8c223cccea47f0e3163f0d328fee3526
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-07-24 16:26:58 +0530

    Time: 3 ms (30.70%) | Memory: 60.1 MB (56.30%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -3,18 +3,19 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function(nums) {
-    let count=0;
-    let maxCount=0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]==0){
-            count=0;
-        }
-        if(nums[i]==1){
-            count+=1;
-        }
-        if(maxCount<count){
-            maxCount=count;
-        }
+    let max_sum =0
+    let count =0
+    let n = nums.length
+    for(let i =0;i<n;i++){
+            if(nums[i]==1){
+                count+=1
+            }
+            if(max_sum<count){
+                max_sum = count
+            }
+            if(nums[i]==0){
+                count=0
+            }
     }
-    return maxCount
+    return max_sum
 };
