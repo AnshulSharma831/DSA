40297840d630a804a507afab677d01b6de5f38e4
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-08-07 11:50:25 +0530

    Time: 0 ms (100.00%) | Memory: 57.9 MB (59.59%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -5,19 +5,20 @@
  */
 var search = function(nums, target) {
     let low=0;
-    let high=nums.length-1;
+    let high=nums.length;
     let mid;
-    while(low<=high){
+    while(low<high){
         mid=Math.floor((low+high)/2);
-        if(nums[mid]<target){
-            low=mid+1;
-        }
-        if(nums[mid]>target){
-            high=mid-1;
-        }
-        if(nums[mid]===target){
+        if(nums[mid]==target){
             return mid;
         }
-    }
-    return -1
+        if(nums[mid]>=target){
+            high=mid;
+        }
+        else{
+            low=mid+1;
+            }
+        }
+    if(low==nums.length || nums[low]!=target) return -1;
+    return low;
 };
