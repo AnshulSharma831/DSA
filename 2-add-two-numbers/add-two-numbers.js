69b15c53743e4c42d5a37b18a45c87873a322321
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-08-30 17:51:01 +0530

    Time: 2 ms (86.27%) | Memory: 61.9 MB (29.36%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -1,35 +1,28 @@
-function ListNode(val, next) {
-    this.val = (val === undefined ? 0 : val);
-    this.next = (next === undefined ? null : next);
-}
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let dummy = new ListNode(0);
-    let current = dummy;
-    let carry = 0;
-
-    while (l1 !== null || l2 !== null || carry !== 0) {
-        let sum = carry;
-        
-        if (l1 !== null) {
-            sum += l1.val;
-            l1 = l1.next;
-        }
-        
-        if (l2 !== null) {
-            sum += l2.val;
-            l2 = l2.next;
-        }
-
-        carry = Math.floor(sum / 10);
-        current.next = new ListNode(sum % 10);
-        current = current.next;
+    let ans=new ListNode();
+    let ansHead=ans;
+    let carry=0;
+    while(l1 || l2 || carry){
+        let sum=(!l1 ? 0:l1.val) + (!l2?0:l2.val) + carry;
+        carry= Math.floor(sum/10);
+        let digit=sum%10;
+        let newnode=new ListNode(digit);
+        ans.next=newnode;
+        ans=ans.next;
+        l1= l1?l1.next:l1;
+        l2= l2?l2.next:l2;
     }
-
-    return dummy.next;
+    return ansHead.next;
 };
