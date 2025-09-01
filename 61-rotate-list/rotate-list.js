d7b1aec96f343342cb905350a48d865722f0fb22
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-09-02 00:26:37 +0530

    Time: 2327 ms (5.02%) | Memory: 57.7 MB (26.99%) - LeetSync

---
+++ 61-rotate-list/rotate-list.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    if(!head || !head.next){
+        return head
+    }
+    if(k==0){
+        return head;
+    }
+    let f=head;
+    while(k>0){
+        if(f.next==null){
+            f=head;
+            k--;
+        }
+        else{
+            f=f.next;
+            k--;
+        }
+    }
+    if(f==head){
+        return head;
+    }
+    let temp=head;
+    while(f.next){
+        f=f.next;
+        temp=temp.next;
+    }
+    let newnode=temp.next;
+    temp.next=null;
+    f.next=head;
+    head=newnode;
+    return head;
+};
