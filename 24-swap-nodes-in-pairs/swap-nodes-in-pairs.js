1e7e9b14a04c465ec625a71c84fbfd3aaf868bdf
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-09-02 12:14:25 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (39.47%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -13,27 +13,20 @@ var swapPairs = function(head) {
     if(!head || !head.next){
         return head;
     }
-    let arr=[];
-    let temp=head;
-    while(temp!=null){
-        arr.push(temp.val);
-        temp=temp.next;
-    }
-    let t;
-    for(let i=0;i<arr.length-1;i+=2){
-        t=arr[i];
-        arr[i]=arr[i+1];
-        arr[i+1]=t;
-    }
+    let curr=head;
+    let temp=curr.next;
     let sent=new ListNode();
-    let ans=sent;
-    let len=arr.length-1;
-    let count=0;
-    while(count<=len){
-        let newnode=new ListNode(arr[count]);
-        ans.next=newnode;
-        ans=ans.next;
-        count++;
+    let prev=sent;
+    while(curr.next && curr){
+        curr.next=temp.next;
+        temp.next=curr;
+        prev.next=temp;
+        prev=temp.next;
+        if(!curr || !curr.next){
+            return sent.next;
+        }
+        curr=curr.next;
+        temp=curr.next;
     }
     return sent.next;
 };
