eed6301116eb70abe1c50d1fbb1a79de3cc37aad
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-09-03 16:22:51 +0530

    Time: 4 ms (5.58%) | Memory: 55 MB (9.02%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -4,7 +4,21 @@
  */
 var lengthOfLastWord = function(s) {
     
-    let str=s.trim();
-    let split=str.split(" ");
-    return split[split.length-1].length;
+    let currsum=0;
+    let sum=0;
+    s=s+" ";
+    let len=s.length;
+    for(let i=0;i<len;i++){
+        if((s[i]==" " && sum!=0)){
+            currsum=sum;
+            sum=0;
+        }
+        else if(s[i]==" "){
+            sum=0;
+        }
+        else{
+            sum+=1;
+        }
+    }
+    return currsum;
 };
