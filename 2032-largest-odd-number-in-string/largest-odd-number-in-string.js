e8ca1b21e3f00b8bc06cc2eb0ae31ced1ed5913e
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-09-17 16:19:21 +0530

    Time: 1 ms (79.81%) | Memory: 59.3 MB (33.71%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -2,10 +2,10 @@
  * @param {string} num
  * @return {string}
  */
-var largestOddNumber = function(s) {
-    for(let i=s.length-1;i>=0;i--){
-        if(parseInt(s[i])%2!=0){
-            return s.slice(0,i+1);
+var largestOddNumber = function(nums) {
+    for(let i=nums.length-1;i>=0;i--){
+        if(parseInt(nums[i])%2!=0){
+            return nums.slice(0,i+1);
         }
     }
     return "";
