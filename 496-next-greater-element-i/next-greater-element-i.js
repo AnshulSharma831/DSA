0c535dcd4626723ac8657d8ee0e087c11737a900
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-10-28 12:10:40 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (40.69%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let ngmap={};
+    let stack=[];
+    let n=nums2.length;
+    stack.push(nums2[n-1]);
+    ngmap[nums2[n-1]]=-1;
+
+    for(let i=n-2;i>=0;i--){
+        let top=stack[stack.length-1];
+        if(nums2[i]<top){
+            ngmap[nums2[i]]=top;
+        }else{
+            while(stack.length){
+                if(stack[stack.length-1]<nums2[i]){
+                    stack.pop();
+                }else{
+                   ngmap[nums2[i]]=stack[stack.length-1];
+                   break; 
+                }
+            }
+            if(stack.length===0){
+                ngmap[nums2[i]]=-1;
+            }
+        }
+        stack.push(nums2[i]);
+    }
+    let ans=[];
+    for(let i=0; i<nums1.length;i++){
+        ans.push(ngmap[nums1[i]]);
+    }
+
+    return ans;
+};
