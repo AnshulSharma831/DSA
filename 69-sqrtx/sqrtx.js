3a15b44d663e4fe007b0b53fc63a70aa70dbf915
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2025-10-31 16:23:06 +0530

    Time: 1 ms (67.55%) | Memory: 54.7 MB (96.30%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -3,5 +3,29 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    return Math.floor(Math.pow(x,0.5));
+    if(typeof x!=="number" || isNaN(x) || x<0){
+        return NaN;
+    }
+    if(x===0 || x===1){
+        return x;
+    }
+    let low=1;
+    let high=x;
+    let result=0;
+
+    while(low<=high){
+        let mid=Math.floor((low+high)/2);
+        let sq=mid*mid;
+        if(sq==x){
+            return mid;
+        }
+        if(sq>x){
+            high=mid-1;
+        }
+        else{
+            result=mid;
+            low=mid+1;
+        }
+    }
+    return result;
 };
