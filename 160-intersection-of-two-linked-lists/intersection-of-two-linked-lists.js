42691a9634c702b8e793818faba8e289f5e96545
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2026-02-11 14:03:14 +0530

    Time: 71 ms (39.25%) | Memory: 65.9 MB (89.03%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -12,17 +12,12 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let hash=new Map();
     let temp=headA;
-    while(temp!=null){
-        hash.set(temp,1);
-        temp=temp.next;
-    }
     let trav=headB;
-    while(trav!=null){
-        if(hash.has(trav)){
-            return trav;
-        }
-        trav=trav.next;
+    while(temp!=trav){
+        temp=temp==null? temp=headB: temp.next;
+        trav=trav==null? trav=headA: trav.next;
+        
     }
+    return temp;
 };
