0e28fcdf0bd56c9dc00cb1dbf7ff7da9088eeb71
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2026-02-11 12:10:22 +0530

    Time: 2 ms (30.05%) | Memory: 56.1 MB (8.53%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,20 +4,44 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+    let lps=[0];
+    let m=needle.length;
     let i=0;
-    let j=0;
-    while(j<haystack.length){
-        if(needle[i]==haystack[j]){
+    let j=1;
+    while(j<m){
+        if(needle[i]==needle[j]){
+            lps[j]=i+1;
             i++;
             j++;
-            if(i>=needle.length){
-            return j-i;
-        }
         }
         else{
-            
-            j=j-i+1;
-            i=0;
+            if(i==0){
+                lps[j]=0;
+                j++;
+            }
+            else{
+                i=lps[i-1]
+            }
+        }
+    }
+    let me=0;
+    let n=0;
+    let h=haystack.length;
+    while(me<h){
+        if(haystack[me]==needle[n]){
+            me++;
+            n++;
+        }
+        else{
+            if(n==0){
+                me++;
+            }
+            else{
+                n=lps[n-1]
+            }
+        }
+        if(n==m){
+            return me-m;
         }
     }
     return -1;
