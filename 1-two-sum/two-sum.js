d75371ab8ad90bd40aaa6154d1e2687f790bbf3d
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2026-02-11 16:30:08 +0530

    Time: 6 ms (45.54%) | Memory: 58.9 MB (5.30%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,14 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map={};
+    let map={}
     for(let i=0;i<nums.length;i++){
-        if(!map[nums[i]]){
-            map[nums[i]]=i;
-        }
+        map[nums[i]]=i;
     }
     for(let i=0;i<nums.length;i++){
-        if(map[target-nums[i]] && map[target-nums[i]]!=i){
+        if(map[target-nums[i]] && map[target-nums[i]] != i){
             return [i,map[target-nums[i]]];
         }
     }
