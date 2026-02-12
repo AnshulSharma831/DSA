96f1ca12377bb2c66a648eb89acc281f7aa4ecf1
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2026-02-12 12:03:25 +0530

    Time: 21 ms (14.59%) | Memory: 60.4 MB (25.79%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -20,9 +20,8 @@ var trap = function(height) {
         if(height[i]>right){
             right=height[i];
         }
-        maxr.push(right);
+        maxr[i]=right;
     }
-    maxr.reverse();
 
     let summ=0;
     for(let i=0;i<maxl.length;i++){
