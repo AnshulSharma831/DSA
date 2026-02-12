7719959898b8965d205a7cc544a25508879a6ecd
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2026-02-12 11:59:50 +0530

    Time: 2 ms (49.41%) | Memory: 59.8 MB (33.83%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let maxl=[]
+    maxl[0]=height[0];
+    let left=height[0];
+    for(let i=0;i<height.length;i++){
+        if(height[i]>left){
+            left=height[i];
+        }
+        maxl.push(left);
+    }
+    let maxr=[];
+    let hlen=height.length-1;
+    maxr[0]=height[hlen];
+    let right=height[hlen];
+    for(let i=hlen-1;i>=0;i--){
+        if(height[i]>right){
+            right=height[i];
+        }
+        maxr.push(right);
+    }
+    maxr.reverse();
+
+    let summ=0;
+    for(let i=0;i<maxl.length;i++){
+        summ+=(Math.min(maxl[i],maxr[i])-height[i])>0 ? Math.min(maxl[i],maxr[i])-height[i] : 0;
+    }
+    return summ;
+};
