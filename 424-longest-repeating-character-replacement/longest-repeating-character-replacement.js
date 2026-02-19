b78219f540652a412d70f6d5efa83c33def041b9
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2026-02-19 11:34:48 +0530

    Time: 7 ms (95.96%) | Memory: 56.1 MB (71.75%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -1,37 +1,24 @@
-/**
- * @param {string} s
- * @param {number} k
- * @return {number}
- */
 var characterReplacement = function(s, k) {
-    
-    let i=j=0;
-    let map={};
-    map[s[0]]=1;
-    let maxcount=0;
-    while(j<s.length){
-        if(windowValid(map,k)){
-            maxcount=Math.max(maxcount,j-i+1);
-            j++;
-            map[s[j]]=!map[s[j]] ? 1 : ++map[s[j]];
-        }
-        else{
-            --map[s[i]];
-            ++i;
-        }
-    }
-    return maxcount;
-};
+    let freq = new Array(26).fill(0);
 
-function windowValid(map,k){
-    let totalcount=0;
-    let maxcount=0;
-    for(let i=0;i<26;i++){
-        let char = String.fromCharCode(65+i);
-        if(map[char]){
-            totalcount+=map[char];
-            maxcount=Math.max(maxcount, map[char])
+    let left = 0;
+    let maxFreq = 0;
+    let maxLen = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        let index = s.charCodeAt(right) - 65;
+        freq[index]++;
+
+        maxFreq = Math.max(maxFreq, freq[index]);
+
+        // If replacements needed > k → shrink window
+        while ((right - left + 1) - maxFreq > k) {
+            freq[s.charCodeAt(left) - 65]--;
+            left++;
         }
+
+        maxLen = Math.max(maxLen, right - left + 1);
     }
-    return ((totalcount-maxcount)<=k)
-}
+
+    return maxLen;
+};
