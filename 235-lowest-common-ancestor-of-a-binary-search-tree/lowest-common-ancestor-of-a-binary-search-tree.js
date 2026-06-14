4965a233d86a10afa04b411ba99f0cfca21bb014
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2026-06-14 19:45:07 +0530

    Time: 985 ms (5.23%) | Memory: 65.8 MB (10.88%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -20,6 +20,13 @@ var lowestCommonAncestor = function(root, p, q) {
         if(!curr){
             return 0;
         }
+        if(p.val<curr.val && q.val<curr.val){
+            let leftans=traverse(curr.left);
+        }
+        else if(p.val>curr.val && q.val>curr.val){
+            let rightans=traverse(curr.right);
+        }
+
         let leftans=traverse(curr.left);
         let rightans=traverse(curr.right);
         if(curr.val==p.val || curr.val==q.val){
