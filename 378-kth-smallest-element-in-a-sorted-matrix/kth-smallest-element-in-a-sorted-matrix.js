0db159165ff221483d51f963be45af7196b40812
Anshul Sharma <147057261+AnshulSharma831@users.noreply.github.com>
2026-07-07 20:32:27 +0530

    Time: 41 ms (44.07%) | Memory: 64 MB (21.11%) - LeetSync

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(matrix, k) {
+    let n=matrix[0].length;
+    let pq=new MinPriorityQueue(x=>x.val);
+    for(let i=0;i<n;i++){
+        pq.push({val:matrix[i][0],row:i,col:0});
+    }
+    for(let count=0;count<k-1;count++){
+        let {val, row, col}=pq.pop();
+        if(col+1<n){
+            pq.push({val:matrix[row][col+1],row:row,col:col+1});
+        }
+    }
+    return pq.pop().val;
+};
